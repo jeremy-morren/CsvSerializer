2c1544ccd5845b7dcb6ba651b1a93563ce16f9ff
Jeremy Morren <jeremy.morren@outlook.com>
2019-03-05 14:31:38 -0400

    Fixed Beta

---
+++ CsvSerializer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
